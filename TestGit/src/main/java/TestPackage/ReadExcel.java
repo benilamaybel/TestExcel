0e00a2a29bbd1a599ab0f7254c048502c00819cb
USER <USER@DESKTOP-56HVALE>
2023-09-12 21:00:11 +0530

    Excel Program

---
+++ TestGit/src/main/java/TestPackage/ReadExcel.java
@@ -0,0 +1,52 @@
+package TestPackage;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ReadExcel {
+	static FileInputStream f; 
+	static XSSFWorkbook book;
+	static XSSFSheet sheet;
+	
+	public static String getStringData(int a, int b) throws IOException   {
+		f = new FileInputStream ("D:\\benila\\Test.xlsx");
+		book = new XSSFWorkbook(f);
+		sheet = book.getSheet("Sheet1");
+		Row r = sheet.getRow(a);
+		Cell c = r.getCell(b);
+		return c.getStringCellValue();
+	}
+	
+	public static int getIntegerData(int a, int b) throws IOException   {
+		f = new FileInputStream ("D:\\benila\\Test.xlsx");
+		book = new XSSFWorkbook(f);
+		sheet = book.getSheet("Sheet1");
+		Row r = sheet.getRow(a);
+		Cell c = r.getCell(b);
+		return (int)c.getNumericCellValue();
+	}
+	
+	public static double getFloatData(int a, int b) throws IOException   {
+		f = new FileInputStream ("D:\\benila\\Test.xlsx");
+		book = new XSSFWorkbook(f);
+		sheet = book.getSheet("Sheet1");
+		Row r = sheet.getRow(a);
+		Cell c = r.getCell(b);
+		return c.getNumericCellValue();
+	}
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		String data = getStringData(0,1);
+		System.out.println(data);
+		int intdata =  getIntegerData(1,0);
+		System.out.println(intdata);
+		double ddata = getFloatData(1,1);
+		System.out.println(ddata);
+	}
+
+}
